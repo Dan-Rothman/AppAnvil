327ddb5123e65270386e46904996aa267a626095
automatrictele <14fyang@gmail.com>
2021-11-12 13:56:40 +0800

    Update loadprofile.cc

--- src/loadprofile.cc
+++ src/loadprofile.cc
@@ -12,21 +12,18 @@ void loadp(std::string fullFileName, std::string& feedBack){
 	std::string fileName;	//name without path
 	fileName = fullFileName.substr(fullFileName.find_last_of("/") + 1);
 
-
 	std::string scmd = "echo \"123\" | sudo mv " + fullFileName + " /etc/apparmor.d";
 	//const char* cmd1 = scmd.c_str();
 	//system(cmd1);
 	system(scmd.c_str());
 
-
 	char line[N];
 	FILE *fp;
 
-	scmd = "echo \"123\" | sudo apparmor_parser -r /etc/apparmor.d/" + fileName;
+	scmd = "echo \"123\" | sudo apparmor_parser -r /etc/apparmor.d/" + fileName + " 2>&1";
 	//const char* cmd2 = scmd.c_str();
 	//fp = popen(cmd2, "r");
 	fp = popen(scmd.c_str(), "r");
-
 
 	//error message
 	std::string fb = "";
